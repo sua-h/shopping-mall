695c564b9a4b55a0d3a05de41bc91c0d2481b7f5
sua-h <suaah.96@gmail.com>
2021-09-10 23:51:15 +0900

    210910

--- src/main/java/com/suah/shoppingmall/configs/WebMvcConfig.java
+++ src/main/java/com/suah/shoppingmall/configs/WebMvcConfig.java
@@ -1,7 +1,7 @@
 package com.suah.shoppingmall.configs;
 
-//import com.suah.interceptors.AttributeInterceptor;
-//import com.suah.interceptors.AutoSignInterceptor;
+import com.suah.shoppingmall.interceptor.AttributeInterceptor;
+import com.suah.shoppingmall.interceptor.AutoSignInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-//    private final AutoSignInterceptor autoSignInterceptor;
-//    private final AttributeInterceptor attributeInterceptor;
-//
-//    @Autowired
-//    public WebMvcConfig(AutoSignInterceptor autoSignInterceptor, AttributeInterceptor attributeInterceptor) {
-//        this.autoSignInterceptor = autoSignInterceptor;
-//        this.attributeInterceptor = attributeInterceptor;
-//    }
+    private final AutoSignInterceptor autoSignInterceptor;
+    private final AttributeInterceptor attributeInterceptor;
+
+    @Autowired
+    public WebMvcConfig(AutoSignInterceptor autoSignInterceptor, AttributeInterceptor attributeInterceptor) {
+        this.autoSignInterceptor = autoSignInterceptor;
+        this.attributeInterceptor = attributeInterceptor;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -26,13 +26,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("/WEB-INF/statics/");
     }
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(this.autoSignInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/**.css", "/**.png", "/**.jpg", "/**.js");
-//        registry.addInterceptor(this.attributeInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/**.css", "/**.png", "/**.jpg", "/**.js");
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.autoSignInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/**.css", "/**.png", "/**.jpg", "/**.js");
+        registry.addInterceptor(this.attributeInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/**.css", "/**.png", "/**.jpg", "/**.js");
+    }
 }
